db31158974134ab75b325b27d2faf5fce5f22647
ClaraPlateiro <clara.plateiro@correo.ucu.edu.uy>
2023-10-24 18:07:57 -0300

    Ejercicio 1

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,23 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"PathToImageToLoad.jpg");
+
+            IPipe pipenull = new PipeNull();
+            picture = pipenull.Send(picture);
+
+            IFilter filter1 = new FilterNegative();
+
+            IPipe pipeserial = new PipeSerial(filter1, pipenull);
+            picture = pipeserial.Send(picture);
+
+            IFilter filter2 = new FilterGreyscale();
+
+            IPipe pipeserial2 = new PipeSerial(filter2, pipeserial);
+            picture = pipeserial2.Send(picture);
+
+            provider.SavePicture(picture, @"PathToImageToSave.jpg");
         }
     }
 }
